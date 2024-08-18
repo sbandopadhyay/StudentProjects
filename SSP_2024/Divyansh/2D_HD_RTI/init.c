249503f560f34580fc6a4ff9617f24b9e0c7a9c2
Somdeb Bandopadhyay <sb020287@gmail.com>
2024-08-18 23:10:18 +0800

    Add files via upload

---
+++ SSP_2024/Divyansh/2D_HD_RTI/init.c
@@ -0,0 +1,202 @@
+/*
+* @Author: Somdeb Bandopadhyay
+* @Date:   2024-08-18
+* @Last Modified by:   somdeb
+* @Last Modified time: 2024-08-18
+*/
+/* ///////////////////////////////////////////////////////////////////// */
+/*! 
+  \file  
+  \brief Contains basic functions for problem initialization.
+
+  The init.c file collects most of the user-supplied functions useful 
+  for problem configuration.
+  It is automatically searched for by the makefile.
+
+  \author A. Mignone (mignone@ph.unito.it)
+  \date   March 5, 2017
+*/
+/* ///////////////////////////////////////////////////////////////////// */
+#include "pluto.h"
+
+#define MY_ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
+/* ********************************************************************* */
+void Init (double *v, double x1, double x2, double x3)
+/*! 
+ * The Init() function can be used to assign initial conditions as
+ * as a function of spatial position.
+ *
+ * \param [out] v   a pointer to a vector of primitive variables
+ * \param [in] x1   coordinate point in the 1st dimension
+ * \param [in] x2   coordinate point in the 2nd dimension
+ * \param [in] x3   coordinate point in the 3rdt dimension
+ *
+ * The meaning of x1, x2 and x3 depends on the geometry:
+ * \f[ \begin{array}{cccl}
+ *    x_1  & x_2    & x_3  & \mathrm{Geometry}    \\ \noalign{\medskip}
+ *     \hline
+ *    x    &   y    &  z   & \mathrm{Cartesian}   \\ \noalign{\medskip}
+ *    R    &   z    &  -   & \mathrm{cylindrical} \\ \noalign{\medskip}
+ *    R    & \phi   &  z   & \mathrm{polar}       \\ \noalign{\medskip}
+ *    r    & \theta & \phi & \mathrm{spherical} 
+ *    \end{array}
+ *  \f]
+ *
+ * Variable names are accessed by means of an index v[nv], where
+ * nv = RHO is density, nv = PRS is pressure, nv = (VX1, VX2, VX3) are
+ * the three components of velocity, and so forth.
+ *
+ *********************************************************************** */
+{
+
+#if PHYSICS == HD
+double dens   =  1.0;
+double drat   =  2.0;
+double damp   =  0.5;
+double pres   =  5.0;
+double ycut   =  0.0;
+double kper   =  1.0;
+double lper   =  0.01;
+double hdel   =  0.005;
+double gacc   = -1.0;
+int nn_x = NX1;
+double xmin = g_domBeg[IDIR];
+double xmax = g_domEnd[IDIR];
+double xlen = xmax - xmin;
+
+double two_pi = 2.0 * M_PI;
+
+double yp = 0.5 * lper * (cos(two_pi * kper * (x1 - xmin) / xlen) +
+                          cos(two_pi * kper * (xmax - x1) / xlen)) + ycut;
+
+
+g_gamma = 1.4;
+
+v[RHO] = dens;
+v[PRS] = pres;
+
+
+
+#if HAVE_ENERGY
+// Case where pressure is set
+if (x2 <= ycut) {
+        v[RHO] = dens;
+} else {
+        v[RHO] = dens * drat;
+}
+
+
+v[RHO] = dens + damp * (1.0 + tanh((x2 - yp) / hdel));
+v[PRS] = pres + v[RHO] * gacc * x2;
+
+
+#else //HAVE_ENERGY
+c_snd=0.1
+c_snd_sq=c_snd*c_snd
+g_isoSoundSpeed =0.1
+        
+// Case where pressure is not set
+if (x2 <= ycut) {
+        v[RHO] = dens * exp(gacc * x2 / c_snd_sq);
+} else {
+        v[RHO] = dens * drat * exp(gacc * x2 / c_snd_sq);
+}
+        
+#endif
+
+
+v[VX1] = v[VX2] = v[VX3] = 0.0;
+
+#endif // PHYSICS == HD
+
+}
+
+/* ********************************************************************* */
+void InitDomain (Data *d, Grid *grid)
+/*! 
+ * Assign initial condition by looping over the computational domain.
+ * Called after the usual Init() function to assign initial conditions
+ * on primitive variables.
+ * Value assigned here will overwrite those prescribed during Init().
+ *
+ *
+ *********************************************************************** */
+{
+}
+
+/* ********************************************************************* */
+void Analysis (const Data *d, Grid *grid)
+/*! 
+ *  Perform runtime data analysis.
+ *
+ * \param [in] d the PLUTO Data structure
+ * \param [in] grid   pointer to array of Grid structures  
+ *
+ *********************************************************************** */
+{
+
+}
+
+/* ********************************************************************* */
+void UserDefBoundary (const Data *d, RBox *box, int side, Grid *grid) 
+/*! 
+ *  Assign user-defined boundary conditions.
+ *
+ * \param [in,out] d  pointer to the PLUTO data structure containing
+ *                    cell-centered primitive quantities (d->Vc) and 
+ *                    staggered magnetic fields (d->Vs, when used) to 
+ *                    be filled.
+ * \param [in] box    pointer to a RBox structure containing the lower
+ *                    and upper indices of the ghost zone-centers/nodes
+ *                    or edges at which data values should be assigned.
+ * \param [in] side   specifies the boundary side where ghost zones need
+ *                    to be filled. It can assume the following 
+ *                    pre-definite values: X1_BEG, X1_END,
+ *                                         X2_BEG, X2_END, 
+ *                                         X3_BEG, X3_END.
+ *                    The special value side == 0 is used to control
+ *                    a region inside the computational domain.
+ * \param [in] grid  pointer to an array of Grid structures.
+ *
+ *********************************************************************** */
+{
+
+}
+
+#if BODY_FORCE != NO
+/* ********************************************************************* */
+void BodyForceVector(double *v, double *g, double x1, double x2, double x3)
+/*!
+ * Prescribe the acceleration vector as a function of the coordinates
+ * and the vector of primitive variables *v.
+ *
+ * \param [in] v  pointer to a cell-centered vector of primitive 
+ *                variables
+ * \param [out] g acceleration vector
+ * \param [in] x1  position in the 1st coordinate direction \f$x_1\f$
+ * \param [in] x2  position in the 2nd coordinate direction \f$x_2\f$
+ * \param [in] x3  position in the 3rd coordinate direction \f$x_3\f$
+ *
+ *********************************************************************** */
+{
+  g[IDIR] = 0.0;
+  g[JDIR] = -1.0;
+  g[KDIR] = 0.0;
+}
+/* ********************************************************************* */
+double BodyForcePotential(double x1, double x2, double x3)
+/*!
+ * Return the gravitational potential as function of the coordinates.
+ *
+ * \param [in] x1  position in the 1st coordinate direction \f$x_1\f$
+ * \param [in] x2  position in the 2nd coordinate direction \f$x_2\f$
+ * \param [in] x3  position in the 3rd coordinate direction \f$x_3\f$
+ * 
+ * \return The body force potential \f$ \Phi(x_1,x_2,x_3) \f$.
+ *
+ *********************************************************************** */
+{
+  return 0.0;
+}
+#endif
